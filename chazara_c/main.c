e4d61ae0b4defa00118d87cc32fd1cbb0730f1f2
Sam Smith <sam@example.com>
2022-11-15 09:47:34 +0200

    ok

--- chazara_c/main.c
+++ chazara_c/main.c
@@ -6,9 +6,8 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main () {
+    
 }
