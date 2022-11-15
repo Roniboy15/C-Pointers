699924b265403b61fd36d83b0e09cb8ee3e2d1f5
Sam Smith <sam@example.com>
2022-11-15 09:42:15 +0200

    Initial Commit

---
+++ chazara_c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  chazara_c
+//
+//  Created by Jaron Treyer on 15.11.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
